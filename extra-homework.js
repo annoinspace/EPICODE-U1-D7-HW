41434bfe1c65dcde622841445668528e86ef6aed
annoinspace <aneesahalmask@gmail.com>
2022-10-05 15:12:49 +0100

    progress up to ex 6

--- extra-homework.js
+++ extra-homework.js
@@ -5,9 +5,9 @@
  The choice should be made based on the value of another variable called isMale.
 */
 
-// let isMale = false
-// gender = isMale === true ? `male` : `female`
-// console.log(gender)
+let isMale = false
+gender = isMale === true ? `male` : `female`
+console.log(gender)
 
 /*EX 2
  Write a piece of code for checking if, given two integers, the value of one of them is 8 or if their addition or subtraction is equal to 8.
@@ -20,9 +20,9 @@ if (a == 8 || b === 8) {
 }
 
 if (a - b === 8) {
-  console.log(`a-b=8`)
+  console.log(`a - b = 8`)
 } else if (b - a === 8) {
-  console.log(`b-a=8`)
+  console.log(`b - a = 8`)
 } else {
   console.log(`The subtraction of numbers does not equal 8`)
 }
@@ -31,7 +31,10 @@ if (a - b === 8) {
  Create a variable and assign to it the concatenation of two strings.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let word1 = `chocolate`
+let word2 = `slipper`
+let mushedWord = word1 + word2
+console.log(mushedWord)
 
 /*EX 4
  Create three variables and assign a numerical value to each one of them. 
@@ -39,17 +42,37 @@ if (a - b === 8) {
  Display the result in the console.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let number1 = 56
+let number2 = 999
+let number3 = 12
+let highTOLow = [number1, number2, number3]
+highTOLow.sort((a, b) => b - a)
+console.log(highTOLow)
 
 /*EX 5
  Write a piece of code for finding the average of two given integers.
 */
+
+let average = (number1 + number2) / 2
+console.log(average)
 
 /* WRITE YOUR ANSWER HERE */
 
 /*EX 6
  Write a piece of code for finding the longest of two given strings.
 */
+
+let string1 = `alsknvlskval`
+let string2 = `lkfgnml`
+
+let length1 = string1.length
+let length2 = string2.length
+
+if (length1 > length2) {
+  console.log(string1 + ` is the longest word`)
+} else {
+  console.log(string2 + ` is the longest word`)
+}
 
 /* WRITE YOUR ANSWER HERE */
 
